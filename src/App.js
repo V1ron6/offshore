a418cd9c34b90e4e83b0d4898b9001f798b39843
Elvin Sly Owusu <mindedbusines@gmail.com>
2025-11-24 04:29:03 +0000

    survice suspende

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React  from 'react';
+import {useState,useEffect} from 'react';
 import './App.css';
 import Top from "./components/top.js";
 import Fxtx from "./components/firsttxt.js";
@@ -9,9 +10,22 @@ import Display from "./components/dispaly.js";
 import Access from "./components/theRest.jsx";
 import Bigger  from "./components/bigger.jsx";
 import Spec from "./components/spec.jsx";
+import Modal from './components/modal.jsx'; 
+
 const App = () =>{
+	const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
  return(
 	<>
+
+	<Modal isOpen={isModalOpen} onClose={closeModal}>
+        <h2>Welcome to the OffShore</h2>
+        <p> we currently undergoing a remodel</p>
+        <button onClick={closeModal}>Close from inside</button>
+      </Modal>
 			<Top />
 			<Fxtx />
 			<Display />
